2556c6167e8ad38442601dfb0d6fe522e0b2ab73
Arthur S Silveira <arthur@fooji.com>
2018-05-10 12:08:03 -0400

    Initial Commit
    W.I.P.

---
+++ src/components/work/Work.js
@@ -0,0 +1,18 @@
+import './Work.css'
+
+import React from 'react'
+
+const Work = () => (
+  <div className='work animated fadeIn'>
+    <div className='work-inner'>
+      <div className='work-section'>
+        <h2 className='section-header'>Coming Soon</h2>
+        <div className='section-content'>
+          This page is under construction!
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Work
