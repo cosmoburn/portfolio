5244fa502f122b8bce5f251b835725c59ed4f268
Arthur S Silveira <arthur@fooji.com>
2018-05-10 15:29:25 -0400

    adding gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import ScrollToTop from './helpers/ScrollToTop.js';
 import './index.css';
 import App from './App';
