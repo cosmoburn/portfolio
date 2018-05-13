4c55aed2da9f1770db15a3eb40b8998072179038
Arthur S Silveira <arthur@fooji.com>
2018-05-13 18:34:26 -0400

    updates postcontact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => (
         <h2 className='section-header'>Get in touch!</h2>
 
         <form className='contact-form' method='POST' action='https://formspree.io/arthur.saboya@gmail.com'>
-          <input type='hidden' name='_next' value='https://arthursilveira.github.io/portfolio'/>
+          <input type='hidden' name='_next' value='https://arthursilveira.github.io/portfolio/#/thanks'/>
           <input type='email' name='_replyto' placeholder='Your email'/>
           <input type='subject' name='_subject' placeholder='Subject'/>
           <textarea name='message' placeholder='How can I help you today?'/>
