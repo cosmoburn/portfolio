30f2a25bdc0503ab53b82294bce2f716e20c12f9
Arthur S Silveira <arthur@fooji.com>
2018-05-13 19:05:57 -0400

    small updates

--- src/components/thanks/Thanks.js
+++ src/components/thanks/Thanks.js
@@ -7,12 +7,12 @@ const Thanks = () => (
       <div className='thanks-section'>
         <h2 className='section-header'> Thanks for reaching out!</h2>
         <div className='section-content'>
-          <p>I got your email and will get back to very soon.</p>
+          <p>I got your email and will get back to you as soon as possible.</p>
         </div>
       </div>
       <div className='thanks-section'>
         <div className='section-content'>
-          <p>If you are too excited and can't wait to get your project going, feel free to call or text me at 859-913-0567.</p>
+          <p>If you are too excited and can't wait to get your project started, feel free to call or text me at 859-913-0567.</p>
         </div>
       </div>
     </div>
