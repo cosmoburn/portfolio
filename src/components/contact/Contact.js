2f36c36a2fc4921f1486aae245657ad515806e9e
Arthur S Silveira <arthur@fooji.com>
2018-05-31 07:21:06 -0400

    test

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -11,7 +11,7 @@ const Contact = () => (
         {/*<input type='hidden' name='next' value='https://arthursilveira.github.io/portfolio/#/thanks'/>*/}
 
         {/* build in form by Netlify are amazing! - https://www.netlify.com/docs/form-handling/ -*/}
-        <form className='contact-form' name='contact-form' method='POST' action='http://arthursaboya.com/thanks' data-netlify='true' data-netlify-honeypot='bot-field'>
+        <form className='contact-form' name='contact-form' method='POST' action='/thanks' data-netlify='true' data-netlify-honeypot='bot-field'>
           <input type='hidden' name='form-name' value='contact-form' />
           <input type='hidden' name='bot-field'/>
           <input type='email' name='email' placeholder='Your email'/>
